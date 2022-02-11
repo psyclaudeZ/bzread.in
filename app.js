e82b6ca37911597e675b103c52245f4cfb51651a
zbw <zbw0517@gmail.com>
2022-02-11 14:51:12 -0800

    correct port number

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
   console.log('Server started on port 3000');
 })
