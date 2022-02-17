1a5e1260076633fe95571f570164c8d8234bfd33
zbw <zbw0517@gmail.com>
2022-02-16 20:02:34 -0800

    revert previous change, ready to upload new zip

--- src/server/login-aws-lambda/service/verify.js
+++ src/server/login-aws-lambda/service/verify.js
@@ -1,7 +1,6 @@
 const {buildResponse, verifyToken} = require('../utils')
 
 function verify(payload) {
-    console.log(`Hitting verify ${JSON.stringify(payload)}`);
     if (!payload || !payload.user || !payload.user.username || !payload.token) {
         return buildResponse(401, {
             message: 'Incorrect request body.'
