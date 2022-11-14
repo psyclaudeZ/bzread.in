833e261ab27156fc3d44e05a9f109db50be6eb1f
zbw <zbw0517@gmail.com>
2022-11-14 12:07:34 -0800

    prettier

--- src/server/login-aws-lambda/service/register.js
+++ src/server/login-aws-lambda/service/register.js
@@ -1,36 +1,40 @@
-const bcrypt = require('bcryptjs');
-const {buildResponse, getUserInfoFromDynamo, saveUserInfoToDynamo} = require('../utils');
+const bcrypt = require("bcryptjs");
+const {
+  buildResponse,
+  getUserInfoFromDynamo,
+  saveUserInfoToDynamo,
+} = require("../utils");
 
 async function register(payload) {
-    const {name, email, username, password} = payload;
+  const { name, email, username, password } = payload;
 
-    if (!name || !email || !username || !password) {
-        return buildResponse(400, {
-            message: 'Missing required information.'
-        })
-    }
+  if (!name || !email || !username || !password) {
+    return buildResponse(400, {
+      message: "Missing required information.",
+    });
+  }
 
-    const dynamoUser = await getUserInfoFromDynamo(username);
-    if (dynamoUser && dynamoUser.username) {
-        return buildResponse(401, {
-            message: 'Username already exists.'
-        })
-    }
+  const dynamoUser = await getUserInfoFromDynamo(username);
+  if (dynamoUser && dynamoUser.username) {
+    return buildResponse(401, {
+      message: "Username already exists.",
+    });
+  }
 
-    // hashSync?
-    const encryptedPassword = await bcrypt.hash(password.trim(), 10);
-    const userEntry = {
-        name,
-        email,
-        username: username.toLowerCase().trim(),
-        password: encryptedPassword,
-    }
+  // hashSync?
+  const encryptedPassword = await bcrypt.hash(password.trim(), 10);
+  const userEntry = {
+    name,
+    email,
+    username: username.toLowerCase().trim(),
+    password: encryptedPassword,
+  };
 
-    const saveUserResponse = await saveUserInfoToDynamo(userEntry);
-    if (!saveUserResponse) {
-        return buildResponse(503, {message: 'Unable to save user.'})
-    }
-    return buildResponse(200, {username})
+  const saveUserResponse = await saveUserInfoToDynamo(userEntry);
+  if (!saveUserResponse) {
+    return buildResponse(503, { message: "Unable to save user." });
+  }
+  return buildResponse(200, { username });
 }
 
-module.exports.register = register
+module.exports.register = register;
