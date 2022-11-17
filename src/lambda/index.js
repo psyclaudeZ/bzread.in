817a7e98c7632571f009d7164b31651597966524
zbw <zbw0517@gmail.com>
2022-11-17 15:52:44 -0800

    add basic AWS lambda logic

---
+++ src/lambda/index.js
@@ -0,0 +1,28 @@
+// AWS JS V2
+const AWS = require("aws-sdk");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+
+exports.handler = async (event, context, callback) => {
+  AWS.config.update({ region: "us-west-1" });
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+  const params = {
+    TableName: "bzreadin-link",
+  };
+  let payload = [];
+  const awsRequest = await ddb.scan(params);
+  const data = await awsRequest.promise();
+
+  console.log(`data: ${JSON.stringify(data)}`);
+  data.Items.forEach(function (elem) {
+    payload.push(unmarshall(elem));
+  });
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      //'Access-Control-Allow-Credentials': true,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+};
