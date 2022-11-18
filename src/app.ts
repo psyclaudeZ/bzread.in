7c275bf1abd1a31ec4b2364ad5544140e7dc48f1
zbw <zbw0517@gmail.com>
2022-11-18 14:06:08 -0800

    reorganized (again!) for introducing a proxy layer

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.get("/posts", (_req, res) => {
       res.writeHead(500);
       console.error(`Error fetching from DB: ${err}`);
     } else {
-      const payload = [];
+      const payload: string[] = [];
       data.Items.forEach(function (elem) {
         payload.push(unmarshall(elem));
       });
