c6e2b3fc963527b2bed711f02afa3246a11ea246
Bowei Zhang <zbw0517@gmail.com>
2023-05-06 11:44:20 -0700

    chore: update the year in footer

--- src/client/src/components/footer.js
+++ src/client/src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="footer">
       <hr></hr>
       <p>
-        © 2022 bzread.in &bull;{" "}
+        © 2023 bzread.in &bull;{" "}
         <a href="mailto:feedback@bzread.in">feedback@bzread.in</a>
       </p>
     </div>
