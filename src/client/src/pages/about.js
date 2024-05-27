1518507c77b327694986dfada4008ec0b415c618
Bowei Zhang <zbw0517@gmail.com>
2024-05-27 15:41:01 +0800

    chore: link to my personal site

--- src/client/src/pages/about.js
+++ src/client/src/pages/about.js
@@ -3,7 +3,10 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <p>bz is bz reading and this service auto-shares his best finds.</p>
+      <p>
+        <a href="https://imbz.online">bz</a> is bz reading and this service auto-shares his best
+        finds.
+      </p>
       <p>expect a new episode every three days.</p>
     </div>
   );
